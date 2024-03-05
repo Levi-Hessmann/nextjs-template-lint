57b973f833194b674388e6b13ce485cb05cf742f
Levi Heßmann <levihsmn@gmail.com>
2024-03-05 12:12:19 +0100

    init

--- src/app/(v1)/route.ts
+++
@@ -1,7 +0,0 @@
-import apiResponse from "@/lib/response";
-
-export async function GET(): Promise<Response> {
-  return apiResponse(200, {
-    message: "Hello, world!",
-  });
-}
--- src/lib/response.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export default function apiResponse(status: number, data: object) {
-  return NextResponse.json({
-    status: status,
-    ...data,
-  });
-}
